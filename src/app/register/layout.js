29afe5c0d3570d81fcb8faefdb9137d019c7a786
Devi-CodingTime <devipurnima26899@gmail.com>
2024-03-21 12:07:33 +0530

    first commit

---
+++ src/app/register/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+      {children}
+      <footer>This is the footer </footer>
+    </div>
+  )
+}
+
+export default layout
